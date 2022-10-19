9f1f79c0df18c7a80ed517db86674eca5289db00
fullxhate <only_one_dream@mail.ru>
2022-10-20 01:56:09 +0300

    fixed hover

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,13 @@
-$(function (){
-  $(document).ready(function () { 
-  $('.header__list header__list--bg').on("click", function () {
-    $('.header__list-hover').fadetoggle();
+$(document).ready(function () {
+  $('.header__list--bg ').click(function () {
+    $('.header__list-hover').slideToggle(300).css('display', 'flex');
+    return false;
+    
   });
 });
+$(document).mouseup(function (e) {
+  var block = $(".header__list-hover");
+  if (!block.is(e.target) && block.has(e.target).length === 0) {
+    block.hide();
+  }
 });
