b9696caca0c334f0501f8248aa453ca7bfcd9e78
fullxhate <only_one_dream@mail.ru>
2022-10-26 02:28:01 +0300

    added

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
   $('.header__list--bg ').click(function () {
     $('.header__list-hover').slideToggle(300).css('display', 'flex');
+    $(".myOverlay").fadeIn(297, function () {
+    });
+    
     return false;
+    
     
   });
 });
@@ -11,3 +15,19 @@ $(document).mouseup(function (e) {
     block.hide();
   }
 });
+
+$(document).ready(function () {
+  $('.header__right-btn ').click(function () {
+    $('.modal').slideToggle(300).css('display', 'flex');
+    return false;
+
+  });
+});
+$(document).mouseup(function (e) {
+  var block = $(".modal");
+  if (!block.is(e.target) && block.has(e.target).length === 0) {
+    block.hide();
+  }
+});
+
+
