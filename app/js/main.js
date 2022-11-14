7fdd61e909ab56a5b2ba8010c7ba2fc2ebbd8e84
fullxhate <only_one_dream@mail.ru>
2022-11-14 23:12:22 +0300

    added filter

--- app/js/main.js
+++ app/js/main.js
@@ -19,6 +19,10 @@ $(function () {
 
 
 
+  // $('.header__list-link--business').on('click', function () {
+  //   $('.header__list-more , .modal-wrapper').css('display', 'flex')
+  //   return false;
+  // });
   $('.header__right-btn ').on('click', function () {
     $('.modal').slideToggle(300), $('.modal-wrapper').fadeIn(297).css('display', 'flex');
     return false;
@@ -95,34 +99,20 @@ $(function () {
     return false;
   });
 
-  // $('.business__img-one').on('click', function () {
-  //   $('.business__items-more--one').fadeIn(297).css('display', 'block'), $('.business__img-one').toggleClass('transform');
-  // $('.business__img-one.transform').on('click', function () {
-  //   $('.business__items-more--one').fadeOut(297).css('display', 'none'), $('.business__img-one').removeClass('transform');
-    
-  // });
-  // });
-  $(".business__items-more--one").slice(0, 3).show();
-  $("body").on('click touchstart', '.business__img-one', function (e) {
-    e.preventDefault();
-    $(".business__items-more--one:hidden").slice(0, 3).slideDown();
-    if ($(".business__items-more--one:hidden").length == 0) {
-      $(".business__img-one").css('visibility', 'hidden');
-    }
-    $('html,body').animate({
-      scrollTop: $(this).offset().top
-    }, 1000);
+  $('.business__img-one').on('click', function () {
+    $('.business__items-more--one ').slideToggle(300);
+    $('.business__items-more--one ').css('display' ,'flex');
+    $('.business__img-one').toggleClass('transform');
   });
 
   $('.business__img-two').on('click', function () {
-    $('.business__items-more--two').slideToggle(300).fadeIn(297).css('display', 'flex'), $('.business__img-two').toggleClass('transform');
-    $('.business__img-two.transform').on('click' , function () {
-      $('.business__items-more--two').fadeOut(297).css('display', 'none'), $('.business__img-two').removeClass('transform');
-      return false;
-    })
-
-    return false;
+    $('.business__items-more--two ').slideToggle(300);
+    $('.business__items-more--two ').css('display' ,'flex');
+    $('.business__img-two').toggleClass('transform');
   });
+
+
+
 
 
 
