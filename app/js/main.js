9f293164eb86af86222a6d33e248827695bc98bf
fullxhate <only_one_dream@mail.ru>
2022-11-28 23:48:30 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,21 @@ $(document).mouseup(function (e) {
     block.hide();
   }
 });
+$(document).ready(function () {
+  $("body").css("display", "none");
+
+  $("body").fadeIn(2000);
+
+  $("a.transition").click(function (event) {
+    event.preventDefault();
+    linkLocation = this.href;
+    $("body").fadeOut(1000, redirectPage);
+  });
+
+  function redirectPage() {
+    window.location = linkLocation;
+  }
+});
 
 
 $(function () {
